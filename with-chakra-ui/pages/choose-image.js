5d20a4cb1f5415a6fb527d1747dcf7a10356bc8c
Stanford Lin <stanlin1999@gmail.com>
2021-02-13 14:25:23 -0800

    Added chakra

--- with-chakra-ui/pages/choose-image.js
+++ with-chakra-ui/pages/choose-image.js
@@ -1,9 +1,54 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbSeparator,
+    Heading,
+    Button,
+    Stack
+} from "@chakra-ui/react";
 
 export default function ChooseImage() {
+
+
     return (
         <>
             <h1>Class Lessons</h1>
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="#">Docs</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem isCurrentPage>
+                    <BreadcrumbLink href="#">Breadcrumb</BreadcrumbLink>
+                </BreadcrumbItem>
+            </Breadcrumb>
+            <Heading>
+                I'm a heading brooooo
+
+            </Heading>
+            <Button colorScheme="blue">
+                LMAOOOO
+            </Button>
+            <Stack spacing={4} direction="row" align="center">
+                <Button colorScheme="teal" size="xs">
+                    Button
+  </Button>
+                <Button colorScheme="teal" size="sm">
+                    Button
+  </Button>
+                <Button colorScheme="teal" size="md">
+                    Button
+  </Button>
+                <Button colorScheme="teal" size="lg">
+                    Button
+  </Button>
+            </Stack>
             <h2>
                 <Link href="/">
                     <a>Back to home</a>
