536227378d950e77e746f62798dc035611531bbd
christy lo <christylo2000@hotmail.com>
2021-02-13 02:48:07 -0800

    routed an extra page

---
+++ with-chakra-ui/pages/choose-image.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function ChooseImage() {
+    return (
+        <>
+            <h1>Class Lessons</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </>
+    )
+}
