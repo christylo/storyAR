56287de0935fa6854e0520f32e7b2fb108a7d136
William <williamzhang8@live.ca>
2021-02-14 01:17:36 -0800

    moved header

---
+++ with-chakra-ui/components/header.js
@@ -0,0 +1,34 @@
+import {Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Flex, Grid, Spacer, Text, useColorMode} from '@chakra-ui/react'
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+export const Header = (props) => {
+    return (
+        <Box>
+            <Grid templateColumns="repeat(3, 2fr)" gap={100}>
+                <Link href="/">
+                    <Text fontSize={48} color="white" fontFamily="BreadCrumb">• storyAR</Text>
+                </Link>
+
+                <Spacer></Spacer>
+                <div className={styles.navTabs}>
+                    <Breadcrumb separator=" " spacing={4} fontFamily="BreadCrumb" fontWeight={50} color="white">
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/">home</BreadcrumbLink>
+                        </BreadcrumbItem>
+
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/create">how does it work</BreadcrumbLink>
+                        </BreadcrumbItem>
+
+                        <BreadcrumbItem>
+                            <BreadcrumbLink href="/create">try it out</BreadcrumbLink>
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+            </Grid>
+        </Box>
+    )
+}
+
+export default Header;
--- with-chakra-ui/pages/create.js
+++ with-chakra-ui/pages/create.js
@@ -12,11 +12,12 @@ import {
     Text,
     Image, Flex
 } from "@chakra-ui/react";
-import { Fonts } from "../components/fonts";
+import {Fonts} from "../components/fonts";
 import socketIOClient from "socket.io-client";
-import { useState } from "react";
+import {useState} from "react";
 import jsCookie from "js-cookie";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
+import Header from '../components/header'
 
 const randomstring = require('randomstring');
 
@@ -54,34 +55,13 @@ export default function Create() {
             <div className={styles.createContainer}>
                 <Head>
                     <title>Create Next App</title>
-                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="icon" href="/favicon.ico"/>
                 </Head>
 
-                <Fonts />
-                <Box>
-                    <Grid templateColumns="repeat(3, 2fr)" gap={100}>
-                        <Link href="/">
-                            <Text fontSize={48} color="white" fontFamily="BreadCrumb">• storyAR</Text>
-                        </Link>
+                <Fonts/>
 
-                        <Spacer></Spacer>
-                        <div className={styles.navTabs}>
-                            <Breadcrumb separator=" " spacing={4} fontFamily="BreadCrumb" fontWeight={50} color="white">
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink href="/">home</BreadcrumbLink>
-                                </BreadcrumbItem>
+                <Header/>
 
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink href="/create">how does it work</BreadcrumbLink>
-                                </BreadcrumbItem>
-
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink href="/create">try it out</BreadcrumbLink>
-                                </BreadcrumbItem>
-                            </Breadcrumb>
-                        </div>
-                    </Grid>
-                </Box>
                 <Flex align="center" justify="center">
                     <Box>
                         <Image src={"/defBackground.svg"}/>
@@ -105,7 +85,8 @@ export default function Create() {
                         create room
                     </Button>
                 </Flex>
-            </div >
+
+            </div>
         </div>
     )
 }
